a06c07ed799bebb815fff1c506203fc6291bf332
Afik Cohen <aphex@aphex.cx>
2013-09-03 02:07:04 -0700

    Fix a bug related to thread creation and print more useful exceptions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 from lxml import etree
 
 namespaces = {'t': 'http://www.titaniumtrack.com/ns/titanium-backup/messages'}
@@ -14,13 +15,14 @@ def merge(xmlfiles):
             txt = f.read()
         root = etree.fromstring(txt)
         threads = root.xpath('t:thread', namespaces=namespaces)
+        print "Loading %s threads from %s ..." % (len(threads), xmlfile)
         for thread in threads:
             address = thread.attrib['address']
             if address not in threadsets:
                 threadsets[address] = set()
             for msg in thread:
                 # drop sms/mms tags into sets organized by address
-                # with the desirable side effect of removing EXACT duplicates
+                # with the desirable side effect of ignoring EXACT duplicates
                 # (messages that share the same text, date, time, everything)
                 threadsets[address].add(etree.tostring(msg))
 
@@ -36,24 +38,38 @@ def merge(xmlfiles):
 def dump(threadsets):
     global THREADS_ELEMENT
     root = THREADS_ELEMENT
+    if root is None:
+        print "Couldn't load the parent 'threads' element, wrong xml format?"
+        return 1
     root.attrib['count'] = str(len(threadsets))
     try:
         for address, threadset in threadsets.iteritems():
+            # build up a thread element
+            thread_elem = etree.Element('thread')
+            thread_elem.attrib['address'] = address
             for msg in threadset:
-                root.append(etree.fromstring(msg))
-    except:
+                # dump all messages into each thread
+                thread_elem.append(etree.fromstring(msg))
+            root.append(thread_elem)
+    except Exception as e:
+        print "Error writing merged XML: "
+        print traceback.format_exc()
+        print e
         return 1
 
     with open('merged.xml', 'w') as outfile:
         outfile.write(etree.tostring(root, encoding='UTF-8',
                                      xml_declaration=True,
                                      standalone=True))
+
+    print "Done, %s threads saved to merged.xml" % len(threadsets)
     return 0
 
 
 def main(argv):
     if len(argv) < 2:
         print "usage: %s xml_file1 xml_file2 ..." % sys.argv[0]
+        return 1
     return dump(merge(argv[1:]))
 
 
